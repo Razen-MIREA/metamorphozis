13879f1b060e1ec2d71a56ca31ff12e65770b308
Skudno <razen.al-nuveyni@mail.ru>
2026-05-02 22:31:44 +0300

    Menu added ~
    - playbutton worked
    - exitbutton worked
    - achivements not worked

---
+++ scripts/menu_buttons.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+func _ready():
+	$ExitButton.pressed.connect(_on_exit_button_pressed)
+	$PlayButton.pressed.connect(_on_load_button_pressed)
+
+# Вызывается при нажатии кнопки "Выход"
+func _on_exit_button_pressed():
+	get_tree().quit() # Завершает работу приложения
+
+# Вызывается при нажатии кнопки "Загрузить"
+func _on_load_button_pressed():
+	# Замените путь на вашу сцену с игрой
+	get_tree().change_scene_to_file("res://Game.tscn")
